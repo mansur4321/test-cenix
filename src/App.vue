0bb9336a21b4565c2bec2a10ba88bd8bbb2d617a
mansur4321 <33467207+mansur4321@users.noreply.github.com>
2024-10-29 11:19:54 +0300

    подключил в app лейауты и написал небольшую ридмишку

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,15 @@
 <script setup lang="ts">
-import { ref, computed, onMounted } from "vue";
-import { usePostsData } from "./stores/index";
-
-const postsData = usePostsData();
-const searchString = ref("");
-
-const searchPosts = computed((): string[] => {
-	return postsData.posts.map((post) => post.name);
-});
-
-onMounted(() => {
-	postsData.getPostsData();
-});
+import HeaderLayout from "./components/Layouts/HeaderLayout.vue";
+import MainLayout from "./components/Layouts/MainLayout.vue";
+import FooterLayout from "./components/Layouts/FooterLayout.vue";
 </script>
 
 <template>
-	<v-app>
-		<v-app-bar
-			:elevation="2"
-			class="pl-8 pr-10"
-		>
-			sdc
-			<template v-slot:prepend>
-				<a
-					href="https://cenix.pro/"
-					target="_blank"
-				>
-					<img
-						width="200"
-						height="50"
-						decoding="async"
-						src="https://cenix.pro/wp-content/uploads/2021/10/logo-1.svg"
-						loading="lazy"
-						alt="cenix.pro icon"
-					/>
-				</a>
-			</template>
+	<v-app class="text-left">
+		<header-layout />
 
-			<v-app-bar-title class="text-left text-h4">Posts</v-app-bar-title>
+		<main-layout />
 
-			<v-autocomplete
-				v-model="searchString"
-				class="mb-n6 w-300px"
-				label="Search"
-				:items="searchPosts"
-				variant="underlined"
-			></v-autocomplete>
-		</v-app-bar>
-
-		<main class="d-flex flex-sm-wrap ga-4"></main>
+		<footer-layout />
 	</v-app>
 </template>
-
-<style scoped></style>
