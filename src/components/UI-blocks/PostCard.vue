935c8f4fea78ce14630f2f1cd2b4548a67b66b6c
mansur4321 <33467207+mansur4321@users.noreply.github.com>
2024-10-30 18:06:34 +0300

    заменил свои иконки Vuetify иконками.

--- src/components/UI-blocks/PostCard.vue
+++ src/components/UI-blocks/PostCard.vue
@@ -67,19 +67,10 @@ const editPost = () => {
 									v-bind="props"
 									@click="toggleMode"
 								>
-									<svg
-										viewBox="0 0 32 32"
-										width="30px"
-										height="30px"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<g>
-											<path
-												:fill="isHovering ? '#fff' : 'rgb(24, 103, 192)'"
-												d="m8.82 25.47a1.48 1.48 0 0 0 .41 0l3.77-1.47a3.68 3.68 0 0 0 1.37-.87l14.22-14.13a2.54 2.54 0 0 0 .26-3.55l-1.29-1.32a2.54 2.54 0 0 0 -3.56.26l-14.19 14.21a3.56 3.56 0 0 0 -.87 1.4l-1.4 3.85a1.38 1.38 0 0 0 .34 1.29 1.31 1.31 0 0 0 .94.33zm17.18-20.95a1.14 1.14 0 0 1 .83.32l1.29 1.29a1.54 1.54 0 0 1 -.26 2.13l-.55.55-3.14-3.16.55-.55a1.88 1.88 0 0 1 1.28-.58zm-16.12 15.78a2.7 2.7 0 0 1 .63-1l13-12.94 3.16 3.16-12.94 13a2.7 2.7 0 0 1 -1 .63l-3.73 1.32a.39.39 0 0 1 -.36-.08.35.35 0 0 1 -.09-.3zm19.62 7.7a.5.5 0 0 1 -.5.5h-26a.5.5 0 0 1 0-1h26a.5.5 0 0 1 .5.5z"
-											></path>
-										</g>
-									</svg>
+									<v-icon
+										icon="mdi-pencil"
+										size="small"
+									></v-icon>
 								</div>
 							</template>
 						</v-tooltip>
@@ -90,30 +81,10 @@ const editPost = () => {
 									v-bind="props"
 									@click="deletePost"
 								>
-									<svg
-										enable-background="new 0 0 32 32"
-										viewBox="0 0 32 32"
-										width="28px"
-										height="28px"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<g id="Layer_1">
-											<g :fill="isHovering ? '#fff' : 'rgb(24, 103, 192)'">
-												<path
-													d="m19.5 23.75c.5522 0 1-.4478 1-1v-10c0-.5522-.4478-1-1-1s-1 .4478-1 1v10c0 .5522.4478 1 1 1z"
-												></path>
-												<path
-													d="m12.5 23.75c.5522 0 1-.4478 1-1v-10c0-.5522-.4478-1-1-1s-1 .4478-1 1v10c0 .5522.4478 1 1 1z"
-												></path>
-												<path
-													d="m20 4.25c.5522 0 1-.4478 1-1s-.4478-1-1-1h-8c-.5522 0-1 .4478-1 1s.4478 1 1 1z"
-												></path>
-												<path
-													d="m5 5.25c-.5522 0-1 .4478-1 1s.4478 1 1 1h1v18.4c0 2.2599 1.84 4.1 4.1 4.1h11.8c2.2599 0 4.1-1.84 4.1-4.1v-18.4h1c.5522 0 1-.4478 1-1s-.4478-1-1-1h-2-18zm19 2v18.4c0 1.16-.94 2.1-2.1 2.1h-11.8c-1.16 0-2.1-.94-2.1-2.1v-18.4z"
-												></path>
-											</g>
-										</g>
-									</svg>
+									<v-icon
+										icon="mdi-delete"
+										size="small"
+									></v-icon>
 								</div>
 							</template>
 						</v-tooltip>
@@ -147,8 +118,8 @@ const editPost = () => {
 
 	&__actions {
 		position: absolute;
-		top: 10px;
-		right: 10px;
+		top: 6px;
+		right: 6px;
 		display: flex;
 		gap: 8px;
 	}
